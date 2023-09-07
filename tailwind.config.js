febf13010c329703ac07ff4647c8e52b05c21941
hmacr <hmac.devo@gmail.com>
2023-09-07 16:44:48 +0530

    setup tailwindcss colors and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    colors: {
+      cyan: 'hsl(178, 100%, 50%)',
+      'soft-blue': 'hsl(215, 51%, 70%)',
+      'dark-blue': {
+        600: 'hsl(215, 32%, 27%)',
+        700: 'hsl(216, 50%, 16%)',
+        800: 'hsl(217, 54%, 11%)',
+      },
+    },
+    fontFamily: {
+      outfit: ['Outfit', ...defaultTheme.fontFamily.serif],
+    },
   },
-  plugins: [],
 };
